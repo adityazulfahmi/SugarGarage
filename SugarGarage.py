dafca918dd9be6183292ed1af4a79388545946ca
adityazulfahmi <adityazulfahmii@gmail.com>
2016-09-26 23:20:01 +0900

    Sugar 1.1

--- SugarGarage.py
+++ SugarGarage.py
@@ -15,28 +15,47 @@ GPIO.setup(RANGE_TRIG,GPIO.OUT)
 GPIO.setup(RANGE_ECHO,GPIO.IN)
 
 while True: 
-  
-  GPIO.output(RANGE_TRIG, False)
-  print "Waiting For Sensor To Settle"
-  time.sleep(1)
+  #temperature part
+  humidity, temperature = Adafruit_DHT.read_retry(sensor, pin) 
+  update_0 = temperature
+  update_1 = humidity
+  if humidity is not None and temperature is not None: 
+    print 'Temp = {0:0.1f}*C '.format(temperature) 
+    print 'Humidity = {0:0.1f}% '.format(humidity) 
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-  GPIO.output(RANGE_TRIG, True)
+    #distance part 
+    GPIO.output(RANGE_TRIG, False)
+    print "Waiting For Sensor To Settle"
+    time.sleep(1)
 
-  time.sleep(0.00001)
-  GPIO.output(RANGE_TRIG, False)
+    GPIO.output(RANGE_TRIG, True)
 
-  while GPIO.input(RANGE_ECHO)==0:
-    pulse_start = time.time()
+    time.sleep(0.00001)
+    GPIO.output(RANGE_TRIG, False)
 
-  while GPIO.input(RANGE_ECHO)==1:
-    pulse_end = time.time()
+    while GPIO.input(RANGE_ECHO)==0:
+      pulse_start = time.time()
 
-  pulse_duration = pulse_end - pulse_start
+    while GPIO.input(RANGE_ECHO)==1:
+      pulse_end = time.time()
 
-  distance = pulse_duration * 17150
+    pulse_duration = pulse_end - pulse_start
 
-  distance = round(distance, 2)
+    distance = pulse_duration * 17150
 
-  print "Distance:",distance,"cm"
+    distance = round(distance, 2)
 
+    print "Distance:",distance,"cm"
+  else: 
+    print 'Failed to get reading. Try again!'
+
+  if update_0 != temperature : 
+    print"##### UPDATE ! ! ! #####"
+
+  if update_1 != humidity : 
+    print"##### UPDATE ! ! ! #####"
+
+  else: 
+    print"######################"
 GPIO.cleanup()
