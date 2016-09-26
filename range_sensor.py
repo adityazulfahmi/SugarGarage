a0770f319d112853f0734f72b61f1d4f82659c79
adityazulfahmi <adityazulfahmii@gmail.com>
2016-09-26 23:52:02 +0900

    Sugar 1.5

--- range_sensor.py
+++ range_sensor.py
@@ -7,32 +7,29 @@ ECHO = 24
 
 print "Distance Measurement In Progress"
 
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
 
-while True: 
-  GPIO.setup(TRIG,GPIO.OUT)
-  GPIO.setup(ECHO,GPIO.IN)
+GPIO.output(TRIG, False)
+print "Waiting For Sensor To Settle"
+time.sleep(2)
 
-  GPIO.output(TRIG, False)
-  print "Waiting For Sensor To Settle"
-  time.sleep(1)
+GPIO.output(TRIG, True)
+time.sleep(0.00001)
+GPIO.output(TRIG, False)
 
-  GPIO.output(TRIG, True)
+while GPIO.input(ECHO)==0:
+  pulse_start = time.time()
 
-  time.sleep(0.00001)
-  GPIO.output(TRIG, False)
+while GPIO.input(ECHO)==1:
+  pulse_end = time.time()
 
-  while GPIO.input(ECHO)==0:
-    pulse_start = time.time()
+pulse_duration = pulse_end - pulse_start
 
-  while GPIO.input(ECHO)==1:
-    pulse_end = time.time()
+distance = pulse_duration * 17150
 
-  pulse_duration = pulse_end - pulse_start
+distance = round(distance, 2)
 
-  distance = pulse_duration * 17150
-
-  distance = round(distance, 2)
-
-  print "Distance:",distance,"cm"
+print "Distance:",distance,"cm"
 
 GPIO.cleanup()
