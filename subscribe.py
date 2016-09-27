0ae06ae403208483e7b0b617a896ea0fd8e15153
adityazulfahmi <adityazulfahmii@gmail.com>
2016-09-27 16:07:46 +0900

    Sugar 2.8

--- subscribe.py
+++ subscribe.py
@@ -13,7 +13,7 @@ def on_message(client, userdata, msg):
 	client.disconnect() 
      
 client = mqtt.Client() 
-client.connect("THE_IP_ADDRESS_OF_OUR_BROKER",1883,60) 
+client.connect("localhost",1883,60) 
 
 client.on_connect = on_connect 
 client.on_message = on_message 
